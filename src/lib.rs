82fab26425269df1f0bc22f7373254dec7018e65
Alexander Cyon <alex.cyon@gmail.com>
2024-10-02 09:27:50 +0200

    FactorInstancesForSpecificNetworkCache

---
+++ src/lib.rs
@@ -0,0 +1,182 @@
+use core::{error, net};
+
+use indexmap::IndexSet;
+use thiserror::Error;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Error)]
+pub enum CommonError {
+    #[error("Error")]
+    Fail,
+}
+
+pub type Result<T, E = CommonError> = std::result::Result<T, E>;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct HiddenConstructor;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct DerivationPath {
+    pub network_id: NetworkID,
+    pub entity_kind: CAP26EntityKind,
+    pub key_kind: CAP26KeyKind,
+    pub entity_index: CAP26EntityIndex,
+}
+impl DerivationPath {
+    pub fn key_space(&self) -> KeySpace {
+        self.entity_index.key_space()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct FactorSourceID(String);
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct HDFactorInstance {
+    pub derivation_path: DerivationPath,
+    pub factor_source_id: FactorSourceID,
+}
+
+/// A FactorInstance with a derivation path that is used for
+/// Account, Unsecurified, TransactionSigning
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct AccountVeci {
+    hidden_constructor: HiddenConstructor,
+    instance: HDFactorInstance,
+}
+impl AccountVeci {
+    pub fn new(instance: HDFactorInstance) -> Result<Self> {
+        let derivation_path = &instance.derivation_path;
+        if derivation_path.entity_kind == CAP26EntityKind::Account
+            && derivation_path.key_space() == KeySpace::Unsecurified
+            && derivation_path.key_kind == CAP26KeyKind::TransactionSigning
+        {
+            Ok(Self {
+                hidden_constructor: HiddenConstructor,
+                instance,
+            })
+        } else {
+            Err(CommonError::Fail)
+        }
+    }
+    pub fn network_id(&self) -> NetworkID {
+        self.instance.derivation_path.network_id.clone()
+    }
+}
+
+/// A FactorInstance with a derivation path that is used for
+/// Identity, Unsecurified, TransactionSigning
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct IdentityVeci {
+    hidden_constructor: HiddenConstructor,
+    instance: HDFactorInstance,
+}
+impl IdentityVeci {
+    pub fn new(instance: HDFactorInstance) -> Result<Self> {
+        let derivation_path = &instance.derivation_path;
+        if derivation_path.entity_kind == CAP26EntityKind::Identity
+            && derivation_path.key_space() == KeySpace::Unsecurified
+            && derivation_path.key_kind == CAP26KeyKind::TransactionSigning
+        {
+            Ok(Self {
+                hidden_constructor: HiddenConstructor,
+                instance,
+            })
+        } else {
+            Err(CommonError::Fail)
+        }
+    }
+    pub fn network_id(&self) -> NetworkID {
+        self.instance.derivation_path.network_id.clone()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum NetworkID {
+    Mainnet,
+    Testnet,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum CAP26EntityKind {
+    Account,
+    Identity,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum CAP26KeyKind {
+    TransactionSigning,
+    AuthenticationSigning,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum CAP26EntityIndex {
+    Securified(u32),
+    Unsecurified(u32),
+}
+impl CAP26EntityIndex {
+    pub fn key_space(&self) -> KeySpace {
+        match self {
+            CAP26EntityIndex::Securified(_) => KeySpace::Securified,
+            CAP26EntityIndex::Unsecurified(_) => KeySpace::Unsecurified,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum KeySpace {
+    Unsecurified,
+    Securified,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum DerivationTemplate {
+    /// Account, Unsecurified, TransactionSigning,
+    /// Veci: Virtual Entity Creating (Factor)Instance
+    AccountVeci,
+
+    /// Identity, Unsecurified, TransactionSigning
+    /// Veci: Virtual Entity Creating (Factor)Instance
+    IdentityVeci,
+
+    /// Account, Securified, AuthenticationSigning
+    AccountRola,
+
+    /// Account, Securified, TransactionSigning
+    AccountMfa,
+
+    /// Identity, Securified, TransactionSigning
+    IdentityMfa,
+}
+
+/// On one specific network
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct FactorInstancesForSpecificNetworkCache {
+    hidden_constructor: HiddenConstructor,
+    pub network: NetworkID,
+    pub unsecurified_accounts: IndexSet<AccountVeci>,
+    pub unsecurified_identities: IndexSet<IdentityVeci>,
+}
+impl FactorInstancesForSpecificNetworkCache {
+    pub fn new(
+        network: NetworkID,
+        unsecurified_accounts: IndexSet<AccountVeci>,
+        unsecurified_identities: IndexSet<IdentityVeci>,
+    ) -> Result<Self> {
+        if unsecurified_accounts
+            .iter()
+            .all(|f| f.network_id() == network)
+            && unsecurified_identities
+                .iter()
+                .all(|f| f.network_id() == network)
+        {
+            Ok(Self {
+                hidden_constructor: HiddenConstructor,
+                network,
+                unsecurified_accounts,
+                unsecurified_identities,
+            })
+        } else {
+            Err(CommonError::Fail)
+        }
+    }
+}
